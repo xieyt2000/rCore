e02241eb6584247b5690a232eb7c6286acc6f9ef
Yuntong Xie <xieyuntong2000@gmail.com>
2021-03-07 22:06:05 +0800

    ch1 exercise code

--- os/src/main.rs
+++ os/src/main.rs
@@ -8,6 +8,9 @@
 mod console;
 mod lang_items;
 mod sbi;
+mod logging;
+
+use log::*;
 
 global_asm!(include_str!("entry.asm"));
 
@@ -34,14 +37,14 @@ pub fn rust_main() -> ! {
         fn boot_stack_top();
     }
     clear_bss();
-    println!("Hello, world!");
-    println!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
-    println!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
-    println!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
-    println!(
+    logging::init();
+    info!(".text [{:#x}, {:#x})", stext as usize, etext as usize);
+    debug!(".rodata [{:#x}, {:#x})", srodata as usize, erodata as usize);
+    error!(".data [{:#x}, {:#x})", sdata as usize, edata as usize);
+    warn!(
         "boot_stack [{:#x}, {:#x})",
         boot_stack as usize, boot_stack_top as usize
     );
-    println!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
+    trace!(".bss [{:#x}, {:#x})", sbss as usize, ebss as usize);
     panic!("Shutdown machine!");
 }
