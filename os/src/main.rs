2aa4cec0a1174b959fa27f46154fa9448897c0f9
Yuntong Xie <xieyuntong2000@gmail.com>
2021-03-20 14:20:10 +0800

    update main from tutorial

--- os/src/main.rs
+++ os/src/main.rs
@@ -3,6 +3,7 @@
 #![feature(global_asm)]
 #![feature(llvm_asm)]
 #![feature(panic_info_message)]
+#![feature(const_in_array_repeat_expressions)]
 
 #[macro_use]
 mod console;
@@ -10,8 +11,10 @@ mod lang_items;
 mod sbi;
 mod syscall;
 mod trap;
-mod batch;
-mod logging;
+mod loader;
+mod config;
+mod task;
+mod timer;
 
 global_asm!(include_str!("entry.asm"));
 global_asm!(include_str!("link_app.S"));
@@ -30,8 +33,11 @@ fn clear_bss() {
 pub fn rust_main() -> ! {
     clear_bss();
     logging::init();
-    log::info!("[kernel] Hello World!s");
+    log::info!("[kernel] Hello World!");
     trap::init();
-    batch::init();
-    batch::run_next_app();
+    loader::load_apps();
+    trap::enable_timer_interrupt();
+    timer::set_next_trigger();
+    task::run_first_task();
+    panic!("Unreachable in rust_main!");
 }
