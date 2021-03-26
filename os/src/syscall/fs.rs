13c28cba6391157aa99111aedb760bb6b16fa63a
Yuntong Xie <xieyuntong2000@gmail.com>
2021-03-27 00:23:01 +0800

    add check for sys_write

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -6,12 +6,16 @@ const FD_STDOUT: usize = 1;
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
     match fd {
         FD_STDOUT => {
-            let buffers = translated_byte_buffer(current_user_token(), buf, len);
-            for buffer in buffers {
-                print!("{}", core::str::from_utf8(buffer).unwrap());
+            if let Some(buffers) = translated_byte_buffer(current_user_token(), buf, len) {
+                for buffer in buffers {
+                    print!("{}", core::str::from_utf8(buffer).unwrap());
+                }
+                len as isize
             }
-            len as isize
-        },
+            else {
+                -1
+            }
+        }
         _ => {
             panic!("Unsupported fd in sys_write!");
         }
