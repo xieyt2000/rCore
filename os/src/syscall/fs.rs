e0359731bf49df14de59ca4327b33ad8d701a12f
Yuntong Xie <xieyuntong2000@gmail.com>
2021-03-27 00:19:32 +0800

    syscall from tutorial

--- os/src/syscall/fs.rs
+++ os/src/syscall/fs.rs
@@ -1,20 +1,19 @@
+use crate::mm::translated_byte_buffer;
+use crate::task::current_user_token;
+
 const FD_STDOUT: usize = 1;
 
 pub fn sys_write(fd: usize, buf: *const u8, len: usize) -> isize {
-    if !crate::loader::user_addr(buf as usize, len) {
-        log::error!("[kernel] sys_write access denied: buf = {:#x}, len = {}", buf as usize, len);
-        return -1;
-    }
     match fd {
         FD_STDOUT => {
-            let slice = unsafe { core::slice::from_raw_parts(buf, len) };
-            let str = core::str::from_utf8(slice).unwrap();
-            print!("{}", str);
+            let buffers = translated_byte_buffer(current_user_token(), buf, len);
+            for buffer in buffers {
+                print!("{}", core::str::from_utf8(buffer).unwrap());
+            }
             len as isize
         },
         _ => {
-            log::error!("[kernel] Unsupported fd in sys_write!");
-            -1
+            panic!("Unsupported fd in sys_write!");
         }
     }
 }
