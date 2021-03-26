855d98277bac141b57dbbd8148ff8fcfbd0b3d87
Yuntong Xie <xieyuntong2000@gmail.com>
2021-03-26 23:55:59 +0800

    trap from tutorial

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -1,14 +1,13 @@
 mod context;
-mod stride;
 mod switch;
 mod task;
 
-use crate::config::{BIG_STRIDE, MAX_APP_NUM, MAX_TIME};
-use crate::loader::{get_num_app, init_app_cx};
-use alloc::collections::binary_heap::BinaryHeap;
+use crate::loader::{get_app_data, get_num_app};
+use crate::mm::{MapPermission, VirtAddr};
+use crate::trap::TrapContext;
+use alloc::vec::Vec;
 use core::cell::RefCell;
 use lazy_static::*;
-use stride::Stride;
 use switch::__switch;
 use task::{TaskControlBlock, TaskStatus};
 
@@ -20,38 +19,26 @@ pub struct TaskManager {
 }
 
 struct TaskManagerInner {
-    tasks: [TaskControlBlock; MAX_APP_NUM],
+    tasks: Vec<TaskControlBlock>,
     current_task: usize,
-    current_stride: usize,
-    heap: BinaryHeap<Stride>,
-    running_slices: [usize; MAX_APP_NUM],
-    priorities: [usize; MAX_APP_NUM],
 }
 
 unsafe impl Sync for TaskManager {}
 
 lazy_static! {
     pub static ref TASK_MANAGER: TaskManager = {
+        info!("init TASK_MANAGER");
         let num_app = get_num_app();
-        let mut tasks = [
-            TaskControlBlock { task_cx_ptr: 0, task_status: TaskStatus::UnInit };
-            MAX_APP_NUM
-        ];
-        let mut heap = BinaryHeap::new();
+        info!("num_app = {}", num_app);
+        let mut tasks: Vec<TaskControlBlock> = Vec::new();
         for i in 0..num_app {
-            tasks[i].task_cx_ptr = init_app_cx(i) as * const _ as usize;
-            tasks[i].task_status = TaskStatus::Ready;
-            heap.push(Stride { stride: 0, pid: i });
+            tasks.push(TaskControlBlock::new(get_app_data(i), i));
         }
         TaskManager {
             num_app,
             inner: RefCell::new(TaskManagerInner {
                 tasks,
                 current_task: 0,
-                current_stride: 0,
-                heap: heap,
-                running_slices: [0; MAX_APP_NUM],
-                priorities: [16; MAX_APP_NUM],
             }),
         }
     };
@@ -59,30 +46,18 @@ lazy_static! {
 
 impl TaskManager {
     fn run_first_task(&self) {
-        let mut inner = self.inner.borrow_mut();
-        let task_to_run = inner.heap.pop().unwrap().pid;
-        inner.current_task = task_to_run;
-        inner.tasks[task_to_run].task_status = TaskStatus::Running;
-        let next_task_cx_ptr2 = inner.tasks[task_to_run].get_task_cx_ptr2();
-        drop(inner);
+        self.inner.borrow_mut().tasks[0].task_status = TaskStatus::Running;
+        let next_task_cx_ptr2 = self.inner.borrow().tasks[0].get_task_cx_ptr2();
         let _unused: usize = 0;
         unsafe {
-            __switch(
-                &_unused as *const _,
-                next_task_cx_ptr2,
-            );
+            __switch(&_unused as *const _, next_task_cx_ptr2);
         }
     }
 
     fn mark_current_suspended(&self) {
         let mut inner = self.inner.borrow_mut();
         let current = inner.current_task;
-        inner.running_slices[current] += 1;
-        inner.tasks[current].task_status = if inner.running_slices[current] >= MAX_TIME {
-            TaskStatus::Exited
-        } else {
-            TaskStatus::Ready
-        };
+        inner.tasks[current].task_status = TaskStatus::Ready;
     }
 
     fn mark_current_exited(&self) {
@@ -91,49 +66,63 @@ impl TaskManager {
         inner.tasks[current].task_status = TaskStatus::Exited;
     }
 
-    fn find_next_task(&self) -> Option<Stride> {
-        self.inner.borrow_mut().heap.pop()
+    fn find_next_task(&self) -> Option<usize> {
+        let inner = self.inner.borrow();
+        let current = inner.current_task;
+        (current + 1..current + self.num_app + 1)
+            .map(|id| id % self.num_app)
+            .find(|id| inner.tasks[*id].task_status == TaskStatus::Ready)
     }
 
-    fn push_current2heap(&self) {
-        let mut inner = self.inner.borrow_mut();
+    fn get_current_token(&self) -> usize {
+        let inner = self.inner.borrow();
         let current = inner.current_task;
-        if inner.tasks[current].task_status == TaskStatus::Ready {
-            let prio = inner.priorities[current];
-            let current_stride = inner.current_stride;
-            inner.heap.push(Stride {
-                stride: current_stride + BIG_STRIDE / prio,
-                pid: current,
-            });
-        }
+        inner.tasks[current].get_user_token()
     }
+
+    fn get_current_trap_cx(&self) -> &mut TrapContext {
+        let inner = self.inner.borrow();
+        let current = inner.current_task;
+        inner.tasks[current].get_trap_cx()
+    }
+
     fn run_next_task(&self) {
-        self.push_current2heap();
         if let Some(next) = self.find_next_task() {
             let mut inner = self.inner.borrow_mut();
             let current = inner.current_task;
-            inner.current_stride = next.stride;
-            let next = next.pid;
             inner.tasks[next].task_status = TaskStatus::Running;
             inner.current_task = next;
             let current_task_cx_ptr2 = inner.tasks[current].get_task_cx_ptr2();
             let next_task_cx_ptr2 = inner.tasks[next].get_task_cx_ptr2();
             core::mem::drop(inner);
             unsafe {
-                __switch(
-                    current_task_cx_ptr2,
-                    next_task_cx_ptr2,
-                );
+                __switch(current_task_cx_ptr2, next_task_cx_ptr2);
             }
         } else {
             panic!("All applications completed!");
         }
     }
-
-    fn set_priority(&self, prio: usize) {
+    fn insert_framed_area(
+        &self,
+        start_va: VirtAddr,
+        end_va: VirtAddr,
+        permission: MapPermission,
+    ) -> Result<(), ()> {
         let mut inner = self.inner.borrow_mut();
         let current = inner.current_task;
-        inner.priorities[current] = prio;
+        inner.tasks[current]
+            .memory_set
+            .insert_framed_area(start_va, end_va, permission)?;
+        Ok(())
+    }
+
+    fn remove_framed_area(&self, start_va: VirtAddr, end_va: VirtAddr) -> Result<(), ()> {
+        let mut inner = self.inner.borrow_mut();
+        let current = inner.current_task;
+        inner.tasks[current]
+            .memory_set
+            .remove_framed_area(start_va, end_va)?;
+        Ok(())
     }
 }
 
@@ -153,10 +142,6 @@ fn mark_current_exited() {
     TASK_MANAGER.mark_current_exited();
 }
 
-pub fn set_priority(prio: usize) {
-    TASK_MANAGER.set_priority(prio);
-}
-
 pub fn suspend_current_and_run_next() {
     mark_current_suspended();
     run_next_task();
@@ -167,6 +152,24 @@ pub fn exit_current_and_run_next() {
     run_next_task();
 }
 
-pub fn get_current_appid() -> usize {
-    TASK_MANAGER.inner.borrow().current_task
+pub fn current_user_token() -> usize {
+    TASK_MANAGER.get_current_token()
+}
+
+pub fn current_trap_cx() -> &'static mut TrapContext {
+    TASK_MANAGER.get_current_trap_cx()
+}
+
+pub fn insert_framed_area(
+    start_va: VirtAddr,
+    end_va: VirtAddr,
+    permission: MapPermission,
+) -> Result<(), ()> {
+    TASK_MANAGER.insert_framed_area(start_va, end_va, permission)?;
+    Ok(())
+}
+
+pub fn remove_framed_area(start_va: VirtAddr, end_va: VirtAddr) -> Result<(), ()> {
+    TASK_MANAGER.remove_framed_area(start_va, end_va)?;
+    Ok(())
 }
