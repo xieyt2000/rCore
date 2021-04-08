7e968e1b0a19dd8e910ced791b0c9eac06e2fc18
Yuntong Xie <xieyuntong2000@gmail.com>
2021-04-08 16:17:39 +0800

    task from tutorial

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -1,138 +1,88 @@
 mod context;
 mod switch;
 mod task;
+mod manager;
+mod processor;
+mod pid;
 
-use crate::loader::{get_num_app, get_app_data};
+use crate::loader::{get_app_data_by_name};
 use crate::mm::{MapPermission, VirtAddr};
-use crate::trap::TrapContext;
-use core::cell::RefCell;
-use lazy_static::*;
 use switch::__switch;
-use task::{TaskControlBlock, TaskStatus};
-use alloc::vec::Vec;
+use task::{TaskControlBlock, TaskStatus};use alloc::sync::Arc;
+use manager::fetch_task;
+use lazy_static::*;
 
 pub use context::TaskContext;
+pub use processor::{
+    run_tasks,
+    current_task,
+    current_user_token,
+    current_trap_cx,
+    take_current_task,
+    schedule,
+};
+pub use manager::add_task;
+pub use pid::{PidHandle, pid_alloc, KernelStack};
 
-pub struct TaskManager {
-    num_app: usize,
-    inner: RefCell<TaskManagerInner>,
+pub fn suspend_current_and_run_next() {
+    // There must be an application running.
+    let task = take_current_task().unwrap();
+
+    // ---- hold current PCB lock
+    let mut task_inner = task.acquire_inner_lock();
+    let task_cx_ptr2 = task_inner.get_task_cx_ptr2();
+    // Change status to Ready
+    task_inner.task_status = TaskStatus::Ready;
+    drop(task_inner);
+    // ---- release current PCB lock
+
+    // push back to ready queue.
+    add_task(task);
+    // jump to scheduling cycle
+    schedule(task_cx_ptr2);
 }
 
-struct TaskManagerInner {
-    tasks: Vec<TaskControlBlock>,
-    current_task: usize,
-}
+pub fn exit_current_and_run_next(exit_code: i32) {
+    // take from Processor
+    let task = take_current_task().unwrap();
+    // **** hold current PCB lock
+    let mut inner = task.acquire_inner_lock();
+    // Change status to Zombie
+    inner.task_status = TaskStatus::Zombie;
+    // Record exit code
+    inner.exit_code = exit_code;
+    // do not move to its parent but under initproc
 
-unsafe impl Sync for TaskManager {}
+    // ++++++ hold initproc PCB lock here
+    {
+        let mut initproc_inner = INITPROC.acquire_inner_lock();
+        for child in inner.children.iter() {
+            child.acquire_inner_lock().parent = Some(Arc::downgrade(&INITPROC));
+            initproc_inner.children.push(child.clone());
+        }
+    }
+    // ++++++ release parent PCB lock here
+
+    inner.children.clear();
+    // deallocate user space
+    inner.memory_set.recycle_data_pages();
+    drop(inner);
+    // **** release current PCB lock
+    // drop task manually to maintain rc correctly
+    drop(task);
+    // we do not have to save task context
+    let _unused: usize = 0;
+    schedule(&_unused as *const _);
+}
 
 lazy_static! {
-    pub static ref TASK_MANAGER: TaskManager = {
-        log::info!("init TASK_MANAGER");
-        let num_app = get_num_app();
-        log::info!("num_app = {}", num_app);
-        let mut tasks: Vec<TaskControlBlock> = Vec::new();
-        for i in 0..num_app {
-            tasks.push(TaskControlBlock::new(
-                get_app_data(i),
-                i,
-            ));
-        }
-        TaskManager {
-            num_app,
-            inner: RefCell::new(TaskManagerInner {
-                tasks,
-                current_task: 0,
-            }),
-        }
-    };
+    pub static ref INITPROC: Arc<TaskControlBlock> = Arc::new(
+        TaskControlBlock::new(get_app_data_by_name("initproc").unwrap())
+    );
 }
 
-impl TaskManager {
-    fn run_first_task(&self) {
-        self.inner.borrow_mut().tasks[0].task_status = TaskStatus::Running;
-        let next_task_cx_ptr2 = self.inner.borrow().tasks[0].get_task_cx_ptr2();
-        let _unused: usize = 0;
-        unsafe {
-            __switch(
-                &_unused as *const _,
-                next_task_cx_ptr2,
-            );
-        }
-    }
-
-    fn mark_current_suspended(&self) {
-        let mut inner = self.inner.borrow_mut();
-        let current = inner.current_task;
-        inner.tasks[current].task_status = TaskStatus::Ready;
-    }
-
-    fn mark_current_exited(&self) {
-        let mut inner = self.inner.borrow_mut();
-        let current = inner.current_task;
-        inner.tasks[current].task_status = TaskStatus::Exited;
-    }
-
-    fn find_next_task(&self) -> Option<usize> {
-        let inner = self.inner.borrow();
-        let current = inner.current_task;
-        (current + 1..current + self.num_app + 1)
-            .map(|id| id % self.num_app)
-            .find(|id| {
-                inner.tasks[*id].task_status == TaskStatus::Ready
-            })
-    }
-
-    fn get_current_token(&self) -> usize {
-        let inner = self.inner.borrow();
-        let current = inner.current_task;
-        inner.tasks[current].get_user_token()
-    }
-
-    fn get_current_trap_cx(&self) -> &mut TrapContext {
-        let inner = self.inner.borrow();
-        let current = inner.current_task;
-        inner.tasks[current].get_trap_cx()
-    }
-
-    fn run_next_task(&self) {
-        if let Some(next) = self.find_next_task() {
-            let mut inner = self.inner.borrow_mut();
-            let current = inner.current_task;
-            inner.tasks[next].task_status = TaskStatus::Running;
-            inner.current_task = next;
-            let current_task_cx_ptr2 = inner.tasks[current].get_task_cx_ptr2();
-            let next_task_cx_ptr2 = inner.tasks[next].get_task_cx_ptr2();
-            core::mem::drop(inner);
-            unsafe {
-                __switch(
-                    current_task_cx_ptr2,
-                    next_task_cx_ptr2,
-                );
-            }
-        } else {
-            panic!("All applications completed!");
-        }
-    }
-    fn insert_framed_area(
-        &self,
-        start: VirtAddr,
-        end: VirtAddr,
-        permission: MapPermission,
-    ) -> Result<(), ()> {
-        let mut inner = self.inner.borrow_mut();
-        let current = inner.current_task;
-        inner.tasks[current].memory_set
-            .insert_framed_area(start, end, permission)?;
-        Ok(())
-    }
-
-    fn remove_framed_area(&self, start: VirtAddr, end: VirtAddr) -> Result<(), ()> {
-        let mut inner = self.inner.borrow_mut();
-        let current = inner.current_task;
-        inner.tasks[current].memory_set
-            .remove_framed_area(start, end)?;
-        Ok(())
-    }
+pub fn add_initproc() {
+    add_task(INITPROC.clone());
 }
 
 pub fn run_first_task() {
@@ -149,24 +99,6 @@ fn mark_current_suspended() {
 
 fn mark_current_exited() {
     TASK_MANAGER.mark_current_exited();
-}
-
-pub fn suspend_current_and_run_next() {
-    mark_current_suspended();
-    run_next_task();
-}
-
-pub fn exit_current_and_run_next() {
-    mark_current_exited();
-    run_next_task();
-}
-
-pub fn current_user_token() -> usize {
-    TASK_MANAGER.get_current_token()
-}
-
-pub fn current_trap_cx() -> &'static mut TrapContext {
-    TASK_MANAGER.get_current_trap_cx()
 }
 
 pub fn insert_framed_area(
