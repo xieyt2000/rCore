9286d73d6f5877bad574a0f738afe48f99d66272
Yuntong Xie <xieyuntong2000@gmail.com>
2021-04-08 16:29:32 +0800

    others from tutorial

--- os/src/main.rs
+++ os/src/main.rs
@@ -44,10 +44,14 @@ pub fn rust_main() -> ! {
     mm::init();
     log::info!("[kernel] back to world!");
     mm::remap_test();
+    task::add_initproc();
+    println!("after initproc!");
     trap::init();
     //trap::enable_interrupt();
     trap::enable_timer_interrupt();
     timer::set_next_trigger();
     task::run_first_task();
+    loader::list_apps();
+    task::run_tasks();
     panic!("Unreachable in rust_main!");
 }
