8775ef5e6435851d623428bba529253e9bd77f98
Yuntong Xie <xieyuntong2000@gmail.com>
2021-05-06 17:07:44 +0800

    fs from tutorial

--- os/src/fs/mod.rs
+++ os/src/fs/mod.rs
@@ -1,9 +1,13 @@
 mod pipe;
 mod stdio;
 mod mail;
+mod inode;
 
 use crate::mm::UserBuffer;
+
 pub trait File : Send + Sync {
+    fn readable(&self) -> bool;
+    fn writable(&self) -> bool;
     fn read(&self, buf: UserBuffer) -> usize;
     fn write(&self, buf: UserBuffer) -> usize;
 }
@@ -11,3 +15,4 @@ pub trait File : Send + Sync {
 pub use pipe::{Pipe, make_pipe};
 pub use stdio::{Stdin, Stdout};
 pub use mail::Mail;
+pub use inode::{OSInode, open_file, OpenFlags, list_apps};
