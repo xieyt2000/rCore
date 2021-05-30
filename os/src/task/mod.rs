83f17bdbc69a13c2964b629dc6e3616446344007
Yuntong Xie <xieyuntong2000@gmail.com>
2021-05-30 21:56:01 +0800

    change initproc name

--- os/src/task/mod.rs
+++ os/src/task/mod.rs
@@ -79,7 +79,7 @@ pub fn exit_current_and_run_next(exit_code: i32) {
 
 lazy_static! {
     pub static ref INITPROC: Arc<TaskControlBlock> = Arc::new({
-        let inode = open_file("initproc", OpenFlags::RDONLY).unwrap();
+        let inode = open_file("ch5_usershell", OpenFlags::RDONLY).unwrap();
         let v = inode.read_all();
         TaskControlBlock::new(v.as_slice())
     });
